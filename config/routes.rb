890ea8306474a3e700995b58389eca6800956159
Brandon Caudillo <btcaudillo@gmail.com>
2023-04-10 09:13:07 -0400

    commit tree bf13b1f365c7595a200f0a0954e8973c048debe1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :recipes
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  # post "/recipes", to: "recipes#show"
+  get "/me", to: "users#show"
+  # get "/recipes", to: "recipes#index"
+  delete "/logout", to: "sessions#destroy"
 end
